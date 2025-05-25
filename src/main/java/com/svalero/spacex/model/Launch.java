07047881d0b2acef6df71f601d1963285908cfff
Alberto <a27768@svalero.com>
2025-05-25 12:35:15 +0200

    Extras added - exports & images

--- src/main/java/com/svalero/spacex/model/Launch.java
+++ src/main/java/com/svalero/spacex/model/Launch.java
@@ -3,6 +3,7 @@ package com.svalero.spacex.model;
 public class Launch {
     private String name;
     private String date_utc;
+    private Links links;
 
     public String getName() {
         return name;
@@ -11,5 +12,25 @@ public class Launch {
     public String getDate_utc() {
         return date_utc;
     }
-}
 
+    public Links getLinks() {
+        return links;
+    }
+
+    public static class Links {
+        private Patch patch;
+
+        public Patch getPatch() {
+            return patch;
+        }
+    }
+
+    public static class Patch {
+        private String small;
+
+        public String getSmall() {
+            return small;
+        }
+    }
+
+}
